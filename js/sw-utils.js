3cb57af5336f28a30c42d112de0a0c97eaf14c0e
ErickGuerron <168254828+ErickGuerron@users.noreply.github.com>
2025-08-03 15:02:44 -0500

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+
+
+
+function actualizaCacheDinamico( dynamiCache, req, res){
+    if( res.ok ) {
+        return caches.open(dynamiCache).then( cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
